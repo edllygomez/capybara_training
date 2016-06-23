cbb9fd8791a8337fcf39618ce670d9f4a1e3f2cf
Christian <cabarca250793@gmail.com>
2016-06-23 02:48:08 -0600

    Finish implementation for test #4

--- spec/support/page_objects/register_prepaid_page.rb
+++ spec/support/page_objects/register_prepaid_page.rb
@@ -18,6 +18,45 @@ module PageObjects
     end
 
     def fill_out_costarricense_form(form_data)
+      fill_in_phone_number(form_data[:prepaid_number])
+      add_number
+      fill_in_id(form_data[:id])
+      fill_in_email(form_data[:email])
+      fill_in_code(form_data[:code])
+    end
+
+    def submit
+      bot.click_by_id(Selector.prepaid_submit_btn)
+    end
+
+    def error_message
+      @error_message ||= bot.text_by(:css, Selector.prepaid_error_msg)
+    end
+
+    private
+
+    def fill_in(selector, text)
+      bot.fill_txt(selector, text)
+    end
+
+    def fill_in_phone_number(phone)
+      fill_in(Selector.prepaid_number_txt, phone)
+    end
+
+    def add_number
+      bot.click_by_id(Selector.prepaid_add_number_btn)
+    end
+
+    def fill_in_id(id)
+      fill_in(Selector.prepaid_id_owner_txt, id)
+    end
+
+    def fill_in_email(email)
+      fill_in(Selector.prepaid_email_txt, email)
+    end
+
+   def fill_in_code(code)
+      fill_in(Selector.prepaid_code_txt, code)
     end
   end
 end
